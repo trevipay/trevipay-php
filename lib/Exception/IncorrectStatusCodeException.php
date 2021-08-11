f2d68a0dfe1bfb811c8b0d57087e8d272b12b585
David Sojevic <dsojevic@multiservice.com>
2021-08-12 09:56:25 +1000

    Packagist stub for dev

--- lib/Exception/IncorrectStatusCodeException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-namespace TreviPay\TreviPay\Exception;
-
-class IncorrectStatusCodeException extends ApiClientException
-{
-}
--- lib/Exception/ResponseException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-namespace TreviPay\TreviPay\Exception;
-
-class ResponseException extends ApiClientException
-{
-}
