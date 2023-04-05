5cea3266b9a5860591dde2ec252654f9e78cf461
DX Team <dx-development-team@trevipay.com>
2023-04-05 23:08:32 +0000

    v1.0.8-develop Release

--- lib/Api/Data/Authorization/UpdateMethod/UpdateAnAuthorizationRequestInterface.php
+++ lib/Api/Data/Authorization/UpdateMethod/UpdateAnAuthorizationRequestInterface.php
@@ -1,6 +1,5 @@
 <?php
 
-declare(strict_types=1);
 
 namespace TreviPay\TreviPay\Api\Data\Authorization\UpdateMethod;
 
--- lib/Model/Data/Charge/CreateMethod/CreateAChargeRequest.php
+++ lib/Model/Data/Charge/CreateMethod/CreateAChargeRequest.php
@@ -1,6 +1,5 @@
 <?php
 
-declare(strict_types=1);
 
 namespace TreviPay\TreviPay\Model\Data\Charge\CreateMethod;
 
